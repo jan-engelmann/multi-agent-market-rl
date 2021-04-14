e379271f79d7523d41cd5d3598062c3e863b801d
enjan <enjan@student.ethz.ch>
2021-04-14 15:36:28 +0200

    small optimizations and starting to implement treatment of observations

--- marl_env/environment.py
+++ marl_env/environment.py
@@ -11,7 +11,7 @@ class Agent:
         self.number = number
 
     def get_action(self, observation: torch.tensor):
-        # time.sleep(0.05)
+        # time.sleep(0.0005)
         return self.number * observation.mean()  # dummy calculation
 
 
@@ -30,6 +30,7 @@ class MultiAgentEnvironment:
         self.n_environments = n_environments  # batch_size
         self.n_features = n_features
 
+        # TODO implement proper agent
         self.sellers = [Agent(num) for num in torch.rand(n_sellers, n_environments)]
         self.buyers = [Agent(num) for num in torch.rand(n_buyers, n_environments)]
 
@@ -40,26 +41,22 @@ class MultiAgentEnvironment:
         self.s_reservations = torch.rand(self.n_sellers)
         self.b_reservations = torch.rand(self.n_buyers)
         self.past_actions = []
-
-    def get_observations(self):
-        # TODO calculate actual observations
-        return torch.rand(self.n_agents, self.n_environments, self.n_features)
+        self.observations = [
+            torch.rand(self.n_agents, self.n_environments, self.n_features)
+        ]  # filling with initial observation
 
     def get_actions(self):
-        observations = self.get_observations()  # n_agents, n_environments, n_features
-
         # TODO improve distributed calculation
         agent_observation_tuples = [
             (agent, obs)
             for agent, obs in zip(
-                itertools.chain(self.sellers, self.buyers), observations
+                itertools.chain(self.sellers, self.buyers), self.observations[-1]
             )
         ]
         res = torch.stack(
             self.worker_pool.starmap(
                 get_agent_actions,
                 agent_observation_tuples,
-                chunksize=self.n_agents // self.worker_pool._processes,
             )
         ).T
         return torch.split(res, [self.n_sellers, self.n_buyers], dim=1)
@@ -87,38 +84,32 @@ class MultiAgentEnvironment:
         b_realized_sorted_deals[no_deal_mask[:, : self.n_buyers]] = 0
 
         # calculating deal prices
-        sorted_deal_prices = (
+        sorted_deal_prices = torch.zeros(self.n_environments, self.max_group_size)
+        sorted_deal_prices[:, : self.max_n_deals] = (
             b_realized_sorted_deals[:, : self.max_n_deals]
             + s_realized_sorted_deals[:, : self.max_n_deals]
         ) / 2.0
 
         # getting the realized prices in the original ordering for buyers&sellers, no deal means 0
-        deals_buyers_orginal = torch.cat(
-            (
-                sorted_deal_prices,
-                torch.zeros(
-                    self.n_environments, max(0, self.n_buyers - self.n_sellers)
-                ),
-            ),
-            dim=1,
-        ).gather(1, b_actions_indices.argsort(1))
+        deals_buyers_orginal = sorted_deal_prices[:, : self.n_buyers].gather(
+            1, b_actions_indices.argsort(1)
+        )
 
-        deals_sellers_orginal = torch.cat(
-            (
-                sorted_deal_prices,
-                torch.zeros(
-                    self.n_environments, max(0, self.n_sellers - self.n_buyers)
-                ),
-            ),
-            dim=1,
-        ).gather(1, s_actions_indices.argsort(1))
+        deals_sellers_orginal = sorted_deal_prices[:, : self.n_sellers].gather(
+            1, s_actions_indices.argsort(1)
+        )
         return deals_sellers_orginal, deals_buyers_orginal
 
     def calculate_rewards(self, deals_sellers, deals_buyers):
-        # calculating rewrds for sellers and buyers
         rewards_sellers = deals_sellers - self.s_reservations[None, :]
         rewards_buyers = self.b_reservations[None, :] - deals_buyers
         return rewards_sellers, rewards_buyers
+
+    def store_observations(self, s_actions, b_actions, deals_sellers, deals_buyers):
+        # TODO actually implement valid observations
+        self.observations.append(
+            torch.rand(self.n_agents, self.n_environments, self.n_features)
+        )
 
     def step(self):
         s_actions, b_actions = self.get_actions()
@@ -126,6 +117,8 @@ class MultiAgentEnvironment:
         rewards_sellers, rewards_buyers = self.calculate_rewards(
             deals_sellers, deals_buyers
         )
+        self.store_observations(s_actions, b_actions, deals_sellers, deals_buyers)
+
         # print(deals_buyers, deals_sellers, rewards_buyers, rewards_sellers, sep="\n")
 
 
@@ -133,10 +126,11 @@ if __name__ == "__main__":
     n_sellers = 50
     n_buyers = 50
     n_environments = 15
-    n_processes = [6]  # [1, 2, 4, 6, 8]
+    n_processes = [1, 2, 4, 6, 8]
     times = []
-    n_iterations = 1
+    n_iterations = 5
     n_features = 100
+    n_steps = 50
 
     for n_proc in tqdm(n_processes, desc="n_processes"):
         for it in tqdm(range(n_iterations), desc="n_iterations", leave=False):
@@ -146,7 +140,8 @@ if __name__ == "__main__":
                 env = MultiAgentEnvironment(
                     n_sellers, n_buyers, n_environments, n_features, pool
                 )
-                env.step()
+                for i in range(n_steps):
+                    env.step()
             end = time.time()
             duration += end - start
         times.append(duration / n_iterations)
