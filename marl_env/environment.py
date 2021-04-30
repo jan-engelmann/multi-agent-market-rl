Loss gradient still NONE! Fixed errors in training loop

marl_env/environment.py
@@ -71,18 +71,19 @@ class MultiAgentEnvironment:
         self.reset()
 
     def reset(self):
-        self.s_reservations = torch.rand(self.n_sellers)
-        self.b_reservations = torch.rand(self.n_buyers)
+        with torch.no_grad():
+            self.s_reservations = torch.rand(self.n_sellers)
+            self.b_reservations = torch.rand(self.n_buyers)
 
-        # Create a mask keeping track of which agent is already done in the current game.
-        self.done_sellers = torch.full(
-            (self.n_environments, self.n_sellers), False, dtype=torch.bool
-        )
-        self.done_buyers = torch.full(
-            (self.n_environments, self.n_buyers), False, dtype=torch.bool
-        )
-        self.newly_finished_sellers = self.done_sellers.clone()
-        self.newly_finished_buyers = self.done_buyers.clone()
+            # Create a mask keeping track of which agent is already done in the current game.
+            self.done_sellers = torch.full(
+                (self.n_environments, self.n_sellers), False, dtype=torch.bool
+            )
+            self.done_buyers = torch.full(
+                (self.n_environments, self.n_buyers), False, dtype=torch.bool
+            )
+            self.newly_finished_sellers = self.done_sellers.clone()
+            self.newly_finished_buyers = self.done_buyers.clone()
 
         self.past_actions = []
         self.observations = []
@@ -126,8 +127,9 @@ class MultiAgentEnvironment:
         # Update the mask keeping track of which agents are done in the current game.
         # This is done with the mask computed in the previous round. Since only agents who were finished since the
         # previous round should get a zero reward.
-        self.done_sellers += self.newly_finished_sellers
-        self.done_buyers += self.newly_finished_buyers
+        with torch.no_grad():
+            self.done_sellers += self.newly_finished_sellers
+            self.done_buyers += self.newly_finished_buyers
 
         self.store_observations()
         s_actions, b_actions = self.get_actions()
@@ -138,14 +140,17 @@ class MultiAgentEnvironment:
         # This results in actions not capable of producing a deal and therefore not interfering with other agents.
         # We make use of element wise multiplication with masking tensors in order to prevent inplace
         # operations (we hope...)
-        s_mask_val = self.b_reservations.max() + 1.0
-        b_mask_val = torch.FloatTensor([0])
+        with torch.no_grad():
+            s_mask_val = self.b_reservations.max() + 1.0
+            b_mask_val = torch.FloatTensor([0])
         s_actions = torch.mul(s_mask_val, self.done_sellers) + torch.mul(s_actions, ~self.done_sellers)
         b_actions = torch.mul(b_mask_val, self.done_buyers) + torch.mul(b_actions, ~self.done_buyers)
 
         deals_sellers, deals_buyers = self.market.step(s_actions, b_actions)
-        self.newly_finished_sellers = deals_sellers > 0
-        self.newly_finished_buyers = deals_buyers > 0
+
+        with torch.no_grad():
+            self.newly_finished_sellers = deals_sellers > 0
+            self.newly_finished_buyers = deals_buyers > 0
 
         rewards_sellers, rewards_buyers = self.calculate_rewards(
             deals_sellers, deals_buyers

marl_env/markets.py
@@ -77,10 +77,12 @@ class MarketMatchHiLo(BaseMarketEngine):
         )  # if true => no deal, if the bid is lower than the offer no deal happens
 
         s_realized_sorted_deals = s_actions_sorted.clone()
-        s_realized_sorted_deals[no_deal_mask[:, : self.n_sellers]] = 0
+        s_realized_sorted_deals = torch.mul(s_realized_sorted_deals, ~no_deal_mask)
+        # s_realized_sorted_deals[no_deal_mask[:, : self.n_sellers]] = 0
 
         b_realized_sorted_deals = b_actions_sorted.clone()
-        b_realized_sorted_deals[no_deal_mask[:, : self.n_buyers]] = 0
+        b_realized_sorted_deals = torch.mul(b_realized_sorted_deals, ~no_deal_mask)
+        # b_realized_sorted_deals[no_deal_mask[:, : self.n_buyers]] = 0
 
         # calculating deal prices
         sorted_deal_prices = torch.zeros(s_actions.shape[0], self.max_group_size)

marl_env/trainer.py
@@ -64,7 +64,7 @@ class ThomasSimpleTrainer:
 
 class MeanAbsErrorTrainer:
     torch.autograd.set_detect_anomaly(True)
-    def __init__(self, env, training_steps=1000):
+    def __init__(self, env, training_steps=5):
         self.env = env
         self.training_steps = training_steps
         self.loss = SimpleLossSetting()
@@ -81,16 +81,15 @@ class MeanAbsErrorTrainer:
             (self.env.n_environments, self.env.n_agents, self.training_steps)
         )
         for t_step in tqdm(range(self.training_steps)):
-
             obs, rew, actions = self.env.step()
             tot_loss = self.loss.get_losses(self.env, rew[0], rew[1])
             tot_loss.backward(torch.full_like(tot_loss, 1.0, dtype=torch.float32), retain_graph=True)
             loss_matrix[:, :, t_step] = tot_loss.detach().numpy()
-            print("Current Loss values are: ")
-            print(loss_matrix)
+            print("The gradient of the Loss is: ")
+            print(tot_loss.grad)
+            print("")
 
             for agent in range(self.env.n_agents):
                 self.optimizers[agent].zero_grad()
                 self.optimizers[agent].step()  # parameter update
-                print(f"Agent {agent} updated")
         return loss_matrix