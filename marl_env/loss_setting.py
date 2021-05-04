Fixed wrong initialisation of n_sellers and n_buyers and masking bug in loss_settings

marl_env/loss_setting.py
@@ -75,7 +75,7 @@ class SimpleLossSetting(LossSetting):
         with torch.no_grad():
             self.s_reservations = torch.mul(s_masking_val, self.done_sellers) + \
                                   torch.mul(self.s_reservations, ~self.done_sellers)
-            self.b_reservations = torch.mul(b_masking_val, self.done_sellers) + \
+            self.b_reservations = torch.mul(b_masking_val, self.done_buyers) + \
                                   torch.mul(self.b_reservations, ~self.done_buyers)
 
             # Compute max reward for each agent and expand to each environment

marl_env/markets.py
@@ -55,9 +55,9 @@ class BaseMarketEngine:
 class MarketMatchHiLo(BaseMarketEngine):
     """
     """
-    def __init__(self, n_buyers, n_sellers, n_environments, max_steps=30):
+    def __init__(self, n_sellers, n_buyers, n_environments, max_steps=30):
         super(MarketMatchHiLo, self).__init__(
-            n_buyers, n_sellers, n_environments, max_steps=30
+            n_sellers, n_buyers, n_environments, max_steps=30
         )
 
     def calculate_deals(self, s_actions, b_actions):
@@ -75,13 +75,12 @@ class MarketMatchHiLo(BaseMarketEngine):
         no_deal_mask = (
             bid_offer_diffs <= 0
         )  # if true => no deal, if the bid is lower than the offer no deal happens
-
         s_realized_sorted_deals = s_actions_sorted.clone()
-        s_realized_sorted_deals = torch.mul(s_realized_sorted_deals, ~no_deal_mask)
+        s_realized_sorted_deals = torch.mul(s_realized_sorted_deals, ~no_deal_mask[:, : self.n_sellers])
         # s_realized_sorted_deals[no_deal_mask[:, : self.n_sellers]] = 0
 
         b_realized_sorted_deals = b_actions_sorted.clone()
-        b_realized_sorted_deals = torch.mul(b_realized_sorted_deals, ~no_deal_mask)
+        b_realized_sorted_deals = torch.mul(b_realized_sorted_deals, ~no_deal_mask[:, : self.n_buyers])
         # b_realized_sorted_deals[no_deal_mask[:, : self.n_buyers]] = 0
 
         # calculating deal prices

marl_env/trainer.py
@@ -85,7 +85,7 @@ class MeanAbsErrorTrainer:
         for t_step in tqdm(range(self.training_steps)):
             obs, rew, actions = self.env.step()
             tot_loss = self.loss.get_losses(self.env, rew[0], rew[1])
-            tot_loss.backward(torch.full_like(tot_loss, 1.0, dtype=torch.float32), retain_graph=True)
+            tot_loss.backward(torch.full_like(tot_loss, 1.0, dtype=torch.float32))
 
             for agent in range(self.env.n_agents):
                 old_params = {}