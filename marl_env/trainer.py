02d060716579bfb0be5bcc1ca6a3e03d1a82207c
Benjamin Suter <ben.suter@bluewin.ch>
2021-05-18 17:24:33 +0200

    Partial implementation of DeepQTrainer - not yet working

--- marl_env/trainer.py
+++ marl_env/trainer.py
@@ -3,63 +3,10 @@ import numpy as np
 
 import torch
 
+from tianshou.data import Batch, ReplayBuffer
 from tqdm import tqdm
 
 from marl_env.loss_setting import SimpleLossSetting
-
-
-class ThomasSimpleTrainer:
-    def __init__(
-        self, env, training_steps=1000, obs_collection_steps=50, discount_factor=0.99
-    ):
-        self.env = env
-        self.training_steps = training_steps
-        self.obs_collection_steps = obs_collection_steps
-        self.discount_coefficients = (discount_factor) ** torch.arange(
-            obs_collection_steps
-        )
-        self.env.reset()
-        self.setup()
-
-    def setup(self):
-        self.optimizers = [
-            torch.optim.Adam(agent.model.parameters()) for agent in self.env.all_agents
-        ]
-
-    def train(self):
-        # rollout experience collection
-        loss_matrix = np.zeros(
-            (self.env.n_agents, self.training_steps, self.obs_collection_steps)
-        )
-        # TODO: understand this loop. Does this make sense?
-        # TODO: make environment break when a game is finished
-        for t_step in tqdm(range(self.training_steps)):
-            all_actions = []
-            all_rewards = []
-            all_obs = []
-            for o_step in range(self.obs_collection_steps):
-                obs, rew, actions = self.env.step()
-                all_actions.append(torch.cat(actions, dim=1))
-                all_rewards.append(torch.cat(rew, dim=1))
-                all_obs.append(obs)
-
-            for agent in range(self.env.n_agents):
-                for i, obs in enumerate(all_obs):
-                    agent_action = self.env.all_agents[agent].get_action(obs[:, agent])
-
-                    # here we use a simple loss, q-value and belmann equation could replace it...
-                    loss = (
-                        -torch.stack(all_rewards[i:])[:, :, agent].sum(-1)
-                        * self.discount_coefficients[i:]
-                    ).sum()  # discount factor goes here
-                    loss.backward()  # gradient calculation
-                    self.optimizers[agent].step()  # parameter update
-                    loss_matrix[
-                        agent, t_step, i
-                    ] = (
-                        loss.detach().numpy()
-                    )  # we should see this dropping as we repeat the training loop.
-        return loss_matrix
 
 
 class MeanAbsErrorTrainer:
@@ -86,8 +33,9 @@ class MeanAbsErrorTrainer:
             (self.env.n_environments, self.env.n_agents, self.training_steps)
         )
         for t_step in tqdm(range(self.training_steps)):
-            obs, rew, actions = self.env.step()
-            tot_loss = self.loss.get_losses(self.env, rew[0], rew[1])
+            obs, act, rew, _, _ = self.env.step()
+            s_rew, b_rew = torch.split(rew, [self.env.n_sellers, self.env.n_buyers], dim=1)
+            tot_loss = self.loss.get_losses(self.env, s_rew, b_rew)
             tot_loss.backward(torch.ones_like(tot_loss))
 
             for agent in range(self.env.n_agents):
@@ -108,3 +56,54 @@ class MeanAbsErrorTrainer:
             loss_matrix[:, :, t_step] = tot_loss.detach().numpy()
 
         return loss_matrix
+
+
+class DeepQTrainer:
+    torch.autograd.set_detect_anomaly(True)
+
+    def __init__(self, env, memory_size, replay_start_size):
+        self.env = env
+        self.env.reset()
+
+        self.buffer = self.set_replay_buffer(memory_size, replay_start_size)
+
+    def set_replay_buffer(self, memory_size, replay_start_size):
+        """
+        Initializes the first N replay buffer entries with random actions.
+        Parameters
+        ----------
+        memory_size: int
+            Total size of the replay buffer
+        replay_start_size: int
+            Number of buffer entries to be initialized with random actions (corresponds to the integer N)
+
+        Returns
+        -------
+        buffer: tianshou.data.ReplayBuffer
+            Initialised replay buffer
+        """
+        buffer = ReplayBuffer(size=memory_size)
+
+        for step in range(replay_start_size):
+            obs, act, rew, obs_next, done = self.env.step(random_action=True)
+            history_batch = Batch(obs=obs, act=act, rew=rew, done=done, obs_next=obs_next)
+            buffer.add(history_batch)
+        return buffer
+
+    def train(self, n_episodes, batch_size):
+        for eps in range(n_episodes):
+            self.env.reset()
+            while not self.env.done:
+                obs, act, rew, obs_next, done = self.env.step()
+                history_batch = Batch(obs=obs, act=act, rew=rew, done=done, obs_next=obs_next)
+                self.buffer.add(history_batch)
+
+                # Sample a random minibatch of transitions from the replay buffer
+                batch_data, indice = self.buffer.sample(batch_size=batch_size)
+                break
+
+
+
+
+
+
