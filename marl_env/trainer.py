09f7f57cf929b14578e57f7415d5e57ed7540adc
Benjamin Suter <ben.suter@bluewin.ch>
2021-05-19 19:38:22 +0200

    Implemented DeepQTrainer

--- marl_env/trainer.py
+++ marl_env/trainer.py
@@ -3,9 +3,10 @@ import numpy as np
 
 import torch
 
+from tqdm import tqdm
+from collections import deque
 from tianshou.data import Batch
 from marl_env.replay_buffer import ReplayBuffer
-from tqdm import tqdm
 
 from marl_env.loss_setting import SimpleLossSetting
 
@@ -62,11 +63,40 @@ class MeanAbsErrorTrainer:
 class DeepQTrainer:
     torch.autograd.set_detect_anomaly(True)
 
-    def __init__(self, env, memory_size, replay_start_size):
+    def __init__(self,
+                 env,
+                 memory_size,
+                 replay_start_size,
+                 update_frq=100,
+                 discount=0.99,
+                 max_loss_history=None,
+                 max_reward_history=None):
+        self.discount = discount
+        self.update_frq = update_frq
+        self.avg_loss_history = deque(maxlen=max_loss_history)
+        self.avg_reward_history = deque(maxlen=max_reward_history)
         self.env = env
         self.env.reset()
 
         self.buffer = self.set_replay_buffer(memory_size, replay_start_size)
+
+    @staticmethod
+    def get_agent_Q_target(agent, observations):
+        target = agent.get_target(observations)
+        return target
+
+    @staticmethod
+    def get_agent_Q_values(agent, observations, actions=None):
+        q_values = agent.get_q_value(observations, actions=actions)
+        return q_values
+
+    @staticmethod
+    def mse_loss(q_targets, q_values):
+        y_target = q_targets.mean(dim=0)
+        prediction = q_values.mean(dim=0)
+
+        loss = torch.clamp(torch.sub(y_target, prediction), -1.0, 1.0).square()
+        return loss
 
     def set_replay_buffer(self, memory_size, replay_start_size):
         """
@@ -90,11 +120,6 @@ class DeepQTrainer:
             history_batch = Batch(obs=obs, act=act, rew=rew, done=done, obs_next=obs_next)
             buffer.add(history_batch)
         return buffer
-
-    @staticmethod
-    def get_agent_Q_target(agent, observation):
-        target = agent.get_target(observation)
-        return target
 
     def generate_Q_targets(self, obs_next, reward, end_of_eps, discount=0.99):
         """
@@ -138,10 +163,31 @@ class DeepQTrainer:
         targets = torch.add(targets, reward.squeeze())
         return targets
 
+    def generate_Q_values(self, obs, act):
+        agent_obs_act_tuples = [
+            (agent, obs_batch.transpose(0, 1), act_batch.transpose(0, 1))
+            for agent, obs_batch, act_batch in zip(
+                self.env.all_agents,
+                obs.squeeze().transpose(0, 1).unsqueeze(1),
+                act.squeeze().transpose(0, 1).unsqueeze(1)
+            )
+        ]
+        q_values = torch.stack(
+            list(
+                itertools.starmap(
+                    DeepQTrainer.get_agent_Q_values,
+                    agent_obs_act_tuples
+                )
+            ),
+            dim=1
+        ).squeeze()
+        return q_values
+
     def train(self, n_episodes, batch_size):
-        # !!! Still being worked on !!!
         for eps in range(n_episodes):
             self.env.reset()
+            eps_loss = deque(maxlen=self.env.market.max_steps)
+            eps_rew = deque(maxlen=self.env.market.max_steps)
             while not self.env.done:
                 obs, act, rew, obs_next, done = self.env.step()
                 history_batch = Batch(obs=obs, act=act, rew=rew, done=done, obs_next=obs_next)
@@ -150,6 +196,35 @@ class DeepQTrainer:
                 # Sample a random minibatch of transitions from the replay buffer
                 batch_data, indice = self.buffer.sample(batch_size=batch_size)
 
-                self.generate_Q_targets(batch_data['obs_next'], batch_data['rew'], batch_data['done'])
-                break
+                q_targets = self.generate_Q_targets(batch_data['obs_next'],
+                                                    batch_data['rew'],
+                                                    batch_data['done'],
+                                                    discount=self.discount).detach()
+                q_values = self.generate_Q_values(batch_data['obs'], batch_data['act'])
+                loss = DeepQTrainer.mse_loss(q_targets, q_values)
+                loss.backward(torch.ones(self.env.n_agents))
 
+                for agent in self.env.all_agents:
+                    # old_params = {}
+                    # for name, param in enumerate(agent.qNetwork.parameters()):
+                    #     old_params[name] = param.clone()
+                    agent.q_opt.step()
+                    agent.q_opt.zero_grad()
+                    # for name, param in enumerate(agent.qNetwork.parameters()):
+                    #     bool_vec = (old_params[name] == param)
+                    #     if False in bool_vec:
+                    #         print("Performed parameter update for agent: ", agent)
+                    #         print("Old values: ", old_params[name][~bool_vec], " New values: ", param[~bool_vec])
+
+                # Monitoring features
+                eps_loss.append(loss)
+                eps_rew.append(rew)
+            avg_loss = torch.stack(list(eps_loss), dim=0).mean(dim=0)
+            avg_rew = torch.stack(list(eps_rew), dim=0).mean(dim=0)
+            self.avg_loss_history.append(avg_loss)
+            self.avg_reward_history.append(avg_rew)
+            if eps % self.update_frq == 0:
+                # print("Updating target Network")
+                for agent in self.env.all_agents:
+                    agent.reset_target_network()
+        return self.avg_loss_history, self.avg_reward_history
