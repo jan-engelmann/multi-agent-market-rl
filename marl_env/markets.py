d82d05a24bdc016c43f5bd5aedda9294d32e862e
Benjamin Suter <ben.suter@bluewin.ch>
2021-05-20 11:50:48 +0200

    removed the n_environments dimension

--- marl_env/markets.py
+++ marl_env/markets.py
@@ -3,14 +3,13 @@ import typing
 
 
 class BaseMarketEngine:
-    def __init__(self, n_sellers, n_buyers, n_environments, max_steps=30):
+    def __init__(self, n_sellers, n_buyers, max_steps=30):
         self.n_sellers = n_sellers
         self.n_buyers = n_buyers
         self.n_agents = n_sellers + n_buyers
         self.max_steps = max_steps
         self.max_group_size = max(n_buyers, n_sellers)
         self.max_n_deals = min(n_buyers, n_sellers)
-        self.n_environments = n_environments
 
         self.time = 0
         self.buyer_history = list()
@@ -20,9 +19,9 @@ class BaseMarketEngine:
     def reset(self):
         """Reset the market to its initial unmatched state."""
         self.time = 0
-        self.buyer_history = list()
-        self.seller_history = list()
-        self.deal_history = list()
+        self.buyer_history.clear()
+        self.seller_history.clear()
+        self.deal_history.clear()
 
     def calculate_deals(
         self, s_actions: torch.Tensor, b_actions: torch.Tensor
@@ -38,14 +37,14 @@ class BaseMarketEngine:
         Parameters
         ----------
         s_actions : torch.Tensor
-            Tensor of seller actions, shape n_environments, n_sellers)
+            Tensor of seller actions, shape (n_sellers,)
         b_actions : torch.Tensor
-            Tensor of buyer actions, shape (n_environments, n_buyers)
+            Tensor of buyer actions, shape (n_buyers,)
 
         Returns
         -------
         typing.Tuple[torch.Tensor torch.Tensor]
-            (deals_sellers, deals_buyers) with shapes (n_environments, n_sellers), (n_environments, n_buyers)
+            (deals_sellers, deals_buyers) with shapes (n_sellers,), (n_buyers,)
         """
         self.seller_history.append(s_actions)
         self.buyer_history.append(b_actions)
@@ -59,9 +58,9 @@ class BaseMarketEngine:
 class MarketMatchHiLo(BaseMarketEngine):
     """
     """
-    def __init__(self, n_sellers, n_buyers, n_environments, max_steps=30):
+    def __init__(self, n_sellers, n_buyers, max_steps=30):
         super(MarketMatchHiLo, self).__init__(
-            n_sellers, n_buyers, n_environments, max_steps=max_steps
+            n_sellers, n_buyers, max_steps=max_steps
         )
 
     def calculate_deals(self, s_actions, b_actions):
@@ -71,37 +70,35 @@ class MarketMatchHiLo(BaseMarketEngine):
         b_actions_sorted, b_actions_indices = b_actions.sort(descending=True)
 
         # get mask for all deals that happen
-        bid_offer_diffs = torch.zeros(s_actions.shape[0], self.max_group_size)
-        bid_offer_diffs[:, : self.max_n_deals] = (
-            b_actions_sorted[:, : self.max_n_deals]
-            - s_actions_sorted[:, : self.max_n_deals]
+        bid_offer_diffs = torch.zeros(self.max_group_size)
+        bid_offer_diffs[: self.max_n_deals] = (
+            b_actions_sorted[: self.max_n_deals]
+            - s_actions_sorted[: self.max_n_deals]
         )
         no_deal_mask = (
             bid_offer_diffs <= 0
         )  # if true => no deal, if the bid is lower than the offer no deal happens
         s_realized_sorted_deals = s_actions_sorted.clone()
-        s_realized_sorted_deals = torch.mul(s_realized_sorted_deals, ~no_deal_mask[:, : self.n_sellers])
-        # s_realized_sorted_deals[no_deal_mask[:, : self.n_sellers]] = 0
+        s_realized_sorted_deals = torch.mul(s_realized_sorted_deals, ~no_deal_mask[: self.n_sellers])
 
         b_realized_sorted_deals = b_actions_sorted.clone()
-        b_realized_sorted_deals = torch.mul(b_realized_sorted_deals, ~no_deal_mask[:, : self.n_buyers])
-        # b_realized_sorted_deals[no_deal_mask[:, : self.n_buyers]] = 0
+        b_realized_sorted_deals = torch.mul(b_realized_sorted_deals, ~no_deal_mask[: self.n_buyers])
 
         # calculating deal prices
-        sorted_deal_prices = torch.zeros(s_actions.shape[0], self.max_group_size)
-        sorted_deal_prices[:, : self.max_n_deals] = (
-            b_realized_sorted_deals[:, : self.max_n_deals]
-            + s_realized_sorted_deals[:, : self.max_n_deals]
+        sorted_deal_prices = torch.zeros(self.max_group_size)
+        sorted_deal_prices[: self.max_n_deals] = (
+            b_realized_sorted_deals[: self.max_n_deals]
+            + s_realized_sorted_deals[: self.max_n_deals]
         ) / 2.0
         self.deal_history.append(sorted_deal_prices)
 
         # getting the realized prices in the original ordering for buyers&sellers, no deal means 0
-        deals_buyers_orginal = sorted_deal_prices[:, : self.n_buyers].gather(
-            1, b_actions_indices.argsort(1)
+        deals_buyers_original = sorted_deal_prices[: self.n_buyers].gather(
+            0, b_actions_indices.argsort().squeeze()
         )
 
-        deals_sellers_orginal = sorted_deal_prices[:, : self.n_sellers].gather(
-            1, s_actions_indices.argsort(1)
+        deals_sellers_orginal = sorted_deal_prices[: self.n_sellers].gather(
+            0, s_actions_indices.argsort().squeeze()
         )
 
-        return deals_sellers_orginal, deals_buyers_orginal
+        return deals_sellers_orginal, deals_buyers_original
