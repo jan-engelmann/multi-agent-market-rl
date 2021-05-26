aca3f6853149dcdae45e43050a69fac023d5c6d2
Benjamin Suter <ben.suter@bluewin.ch>
2021-05-26 15:58:27 +0200

    Implemented HumanReplayAgent

--- marl_env/agents.py
+++ marl_env/agents.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import pandas as pd
 
 
 class AgentSetting:
@@ -15,6 +16,10 @@ class AgentSetting:
         self.reservation = reservation
         self.in_features = in_features
 
+        # Print role and reservation price of the agent being initialised
+        print(f"-- role: {role}")
+        print(f"-- reservation: {reservation}")
+
         # For Buyer: Action space is the closed interval of [min_s_reservation, self.reservation]
         #     Where min_s_reservation denotes the smallest reservation price of all sellers. Therefore this action will
         #     result in 'no action' since a buyer must always bide higher then the reservation price of a seller.
@@ -27,8 +32,14 @@ class AgentSetting:
         #     The lowest possible action is given by the reservation price of a seller guaranteeing a minimal profit
         #     of 0.5 in case a deal is reached.
         if role == "buyer":
+            assert action_boundary < (reservation + 1), "Reservation price of buyer is <= the smallest reservation " \
+                                                        "price of all sellers. This results in an empty action space." \
+                                                        "Are you sure you wanted to make a buyer?"
             self.action_space = np.arange(action_boundary, reservation + 1)
         else:
+            assert reservation < (action_boundary + 1), "Reservation price of seller is >= the largest reservation " \
+                                                        "price of all buyers. This results in an empty action space." \
+                                                        "Are you sure you wanted to make a seller?"
             self.action_space = np.arange(reservation, action_boundary + 1)
 
     def get_action(self, observation, epsilon=0.05):
@@ -73,11 +84,13 @@ class DQNAgent(AgentSetting):
             q_lr: float, optional (default=0.001)
                 Learning rate provided to the Q-Network optimizer
         """
+        print("Initialising DQNAgent")
         super(DQNAgent, self).__init__(role, reservation, in_features, action_boundary)
 
         self.qNetwork = None
         self.targetNetwork = None
         self.q_opt = None
+        lr = kwargs.pop("lr", 0.001)
 
         # Number of out_features is equal to the number of possible actions. Therefore the number of out_features is
         # given by the length of the action_space.
@@ -85,7 +98,11 @@ class DQNAgent(AgentSetting):
         self.set_q_network(self.in_features, out_features)
         self.set_target_network(self.in_features, out_features)
         self.reset_target_network()
-        self.set_optimizers(kwargs.pop("q_lr", 0.001))
+        self.set_optimizers(lr)
+
+        # Print optional agent settings
+        print(f"-- lr: {lr}")
+        print("")
 
     def set_q_network(self, in_features, out_features):
         # in_features is determined by the info_setting --> How many features does the agent see.
@@ -171,7 +188,7 @@ class DQNAgent(AgentSetting):
         max_q: torch.Tensor
             Tensor containing all Q values. Has shape (batch_size, 1)
         """
-        q_values = self.qNetwork(observation)
+        q_values = self.qNetwork(observation).squeeze()
         if torch.is_tensor(actions):
             indices = torch.stack(
                 [torch.tensor(self.action_space) == act for act in actions]
@@ -193,7 +210,7 @@ class DQNAgent(AgentSetting):
         -------
 
         """
-        q_values = self.targetNetwork(observation)
+        q_values = self.targetNetwork(observation).squeeze()
         max_q, _ = torch.max(q_values, dim=1)
 
         # Get Q-values corresponding to 'no action'
@@ -215,6 +232,7 @@ class DQNAgent(AgentSetting):
 
 class ConstAgent(AgentSetting):
     def __init__(self, role, reservation, in_features, action_boundary, **kwargs):
+        print("Initialising ConstAgent")
         super(ConstAgent, self).__init__(
             role, reservation, in_features, action_boundary
         )
@@ -222,21 +240,138 @@ class ConstAgent(AgentSetting):
         self.const_price = kwargs.pop(
             "const_price", (reservation + action_boundary) // 2.0
         )
+
+        # Print optional agent settings
+        print(f"-- const_price: {self.const_price}")
+
+        assert self.const_price in self.action_space, f"The chosen constant price {self.const_price} is not included " \
+                                                      f"in the action space! Possible integer constant prices must " \
+                                                      f"be in the interval " \
+                                                      f"[{self.action_space.min()}, {self.action_space.max()}]"
         self.q_opt = self.Optimizer()
+        print("")
 
     def get_action(self, observation, epsilon=0.05):
+        """
+        Returns the constant action price of the agent
+        Returns
+        -------
+        torch.Tensor of shape (1,) containing the action price of the agent
+        """
         return torch.Tensor([self.const_price])
 
     def get_target(self, observation, agent_state=None):
+        """
+        Dummy function --> ConstAgent is a zero intelligence agent
+        """
         return torch.zeros((observation.shape[0])).unsqueeze(1)
 
     def get_q_value(self, observation, actions=None):
+        """
+        Dummy function --> ConstAgent is a zero intelligence agent
+        """
         return torch.zeros((observation.shape[0]))
 
     def reset_target_network(self):
         pass
 
     class Optimizer:
+        """
+        Dummy optimizer
+        """
+        def __init__(self):
+            pass
+
+        def step(self):
+            pass
+
+        def zero_grad(self):
+            pass
+
+
+class HumanReplayAgent(AgentSetting):
+    def __init__(self, role, reservation, in_features, action_boundary, **kwargs):
+        print("Initialising HumanReplayAgent")
+        super(HumanReplayAgent, self).__init__(
+            role, reservation, in_features, action_boundary
+        )
+        data_type = kwargs.pop("data_type", "new_data")
+        treatment = kwargs.pop("treatment", "FullLimS")
+        player_id = kwargs.pop("id", 954)
+
+        # Make better...
+        if data_type == "new_data":
+            data_path = "../HumanReplayData/NewData/new_data.csv"
+        else:
+            data_path = "../HumanReplayData/NewData/old_data.csv"
+
+        # Print optional agent settings
+        print(f"-- data_path: {data_path}")
+        print(f"-- treatment: {treatment}")
+        print(f"-- id: {player_id}")
+
+        data = pd.read_csv(data_path)
+        data = data.dropna(axis=0, subset=["valuation"])
+
+        # Assert that the correct reservation price is used in the market.
+        tmp = data.loc[(data["treatment"] == treatment) & (data["id"] == player_id)][
+            "valuation"
+        ].tolist()
+        assert tmp == len(tmp) * [reservation], (
+            "Reservation price from data does not match with the reservation "
+            "price from the agent dict."
+        )
+        # Assert that the agent has the correct role in the market.
+        tmp = data.loc[(data["treatment"] == treatment) & (data["id"] == player_id)][
+            "side"
+        ].tolist()
+        assert [x.lower() for x in tmp] == len(tmp) * [role], "Role from data set does not match with the role from " \
+                                                              "the agent dict"
+
+        self.action_list = data.loc[
+            (data["treatment"] == treatment) & (data["id"] == player_id)
+        ]["bid"].tolist()
+        assert len(self.action_list) != 0, "Action list is empty --> Probably the chosen HumanReplayAgent does not " \
+                                           "exist. Double check the chosen agent configurations: 'data_type':... " \
+                                           "'treatment':..., 'id':..."
+
+        self.q_opt = self.Optimizer()
+        self.action = None
+        print("")
+
+    def get_action(self, observation, epsilon=0.05):
+        """
+        Returns the next bid from the data set for every market step. If all data steps have been used, the last
+        available bid from the data set will be returned.
+        Returns
+        -------
+        torch.Tensor of shape (1,) containing the bid price of the agent
+        """
+        try:
+            self.action = self.action_list.pop(0)
+        except IndexError:
+            pass
+        return torch.Tensor([self.action])
+
+    def get_target(self, observation, agent_state=None):
+        """
+        Dummy function --> HumanReplayAgent is a zero intelligence agent
+        """
+        return torch.zeros((observation.shape[0])).unsqueeze(1)
+
+    def get_q_value(self, observation, actions=None):
+        """
+        Dummy function --> HumanReplayAgent is a zero intelligence agent
+        """
+        return torch.zeros((observation.shape[0]))
+
+    def reset_target_network(self):
+        pass
+
+    class Optimizer:
+        """
+        Dummy optimizer
+        """
         def __init__(self):
             pass
 
